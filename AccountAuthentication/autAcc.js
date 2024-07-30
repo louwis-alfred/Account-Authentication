952b2186b4a83546fa4b95dd961b6ad3791e3deb
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-07-31 01:02:25 +0800

    Update autAcc.js

--- AccountAuthentication/autAcc.js
+++ AccountAuthentication/autAcc.js
@@ -36,7 +36,7 @@ const auth = {
 
 // Example usage:
 console.log(auth.register('macarthur', 'password123')); // User registered successfully
-console.log(auth.login('louwis', 'password123'));    // Login successful
+console.log(auth.login('macarthur', 'password123'));    // Login successful
 console.log(auth.logout());                          // User logged out
 console.log(auth.login('louwis', 'wrongpassword'));  // Incorrect password
 console.log(auth.login('kapitan', 'password123'));  // Username does not exist
